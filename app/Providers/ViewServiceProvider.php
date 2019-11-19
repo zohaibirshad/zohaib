d36a766611c7c8d844138aa62b9690b549126b55
Emmanuel Fache <emrade95@gmail.com>
2019-11-19 17:17:23 +0000

    Sidebar Data Now Dynamic

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Bid;
+use App\Models\Invite;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Job;
 use App\Models\Profile;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Support\Facades\Auth;
 
 class ViewServiceProvider extends ServiceProvider
@@ -27,19 +30,31 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $user = Auth::user();
-
-        if (!empty($user)) {
-            $freelancer = Profile::where('user_id', $user->id)->first();
-
-            if ($user->hasRole('hirer')) { 
-                $ongoingJobsCount = Job::where('user_id',  $freelancer->id)->where('status', 'assigned')->count();
-            } else {
-                $ongoingJobsCount = Job::where('profile_id',  $freelancer->id)->where('status', 'assigned')->count();
+        View::composer('partials.dashboard_sidebar', function ($view) {
+            $user = Auth::user();
+            if (!empty($user)) {
+                $freelancer = Profile::where('user_id', $user->id)->first();
+    
+                if ($user->hasRole('hirer')) { 
+                    $newJobsCount = Job::where('user_id',  $freelancer->id)->where('status', 'not assigned')->count();
+                    $ongoingJobsCount = Job::where('user_id',  $freelancer->id)->where('status', 'assigned')->count();
+                    $completedJobsCount = Job::where('user_id',  $freelancer->id)->where('status', 'completed')->count();
+                    $bidsCount = 0;
+                    $invitesCount = Invite::where('user_id', $freelancer->id)->count();
+                } else {
+                    $newJobsCount = 0;
+                    $ongoingJobsCount = Job::where('profile_id',  $freelancer->id)->where('status', 'assigned')->count();
+                    $completedJobsCount = Job::where('profile_id',  $freelancer->id)->where('status', 'completed')->count();
+                    $bidsCount = Bid::where('profile_id', $freelancer->id)->count();
+                    $invitesCount = Invite::where('profile_id', $freelancer->id)->count();
+                }
+                
             }
-            View::composer('partials.dashboard_sidebar', function ($view) use ($ongoingJobsCount) {
-                $view->with('ongoing_jobs_count', $ongoingJobsCount);
-            });
-        }
+            $view->with('ongoing_jobs_count', $ongoingJobsCount);
+            $view->with('completed_jobs_count', $completedJobsCount);
+            $view->with('bids_count', $bidsCount);
+            $view->with('invites_count', $invitesCount);
+            $view->with('new_jobs_count', $newJobsCount);
+        });
     }
 }
