23af8542b4ac38e5fd57b4f1f42f92956ae5676a
Emmanuel Oduro <emmarthurson@gmail.com>
2019-12-02 13:21:36 +0000

    hotfix

---
+++ database/migrations/2019_12_02_015358_add_plan_id_to_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlanIdToPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('plan', function (Blueprint $table) {
+            $table->string('plan_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->dropColumn('plan_id');
+        });
+    }
+}
