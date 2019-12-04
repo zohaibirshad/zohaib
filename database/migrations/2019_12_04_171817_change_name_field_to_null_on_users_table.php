90cfd17d66a14cf9277a6bd4b8bdc8faccbb5349
Emmanuel Oduro <emmarthurson@gmail.com>
2019-12-04 15:12:32 +0000

    hotfix

--- database/migrations/2019_12_04_171817_change_name_field_to_null_on_users_table.php
+++ database/migrations/2019_12_04_171817_change_name_field_to_null_on_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToUsersTable extends Migration
+class ChangeNameFieldToNullOnUsersTable extends Migration
 {
     /**
      * Run the migrations.
