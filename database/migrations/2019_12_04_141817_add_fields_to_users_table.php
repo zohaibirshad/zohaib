8ddb24896fd2c35ec6e1472bc42be36275e6606e
Emmanuel Oduro <emmarthurson@gmail.com>
2019-12-04 14:18:47 +0000

    hotfix

---
+++ database/migrations/2019_12_04_141817_add_fields_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->nullable('first_name')->string();
+            $table->nullable('last_name')->string();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+        });
+    }
+}
