7a51966a6f83e8cc2fc65dc115c3c3df79b644ae
Emmanuel Oduro <emmarthurson@gmail.com>
2019-12-09 19:51:06 +0000

    hotfix

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,11 +48,8 @@ class User extends Authenticatable implements MustVerifyEmail, Auditable
         'email_verified_at' => 'datetime',
     ];
     
-    protected $appends = [
-        'name'
-    ];
 
-    
+
     public function getNameAttribute() 
     {
         return "{$this->first_name} {$this->last_name}";
