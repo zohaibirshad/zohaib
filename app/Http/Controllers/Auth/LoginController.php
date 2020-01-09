aceb3bb5fb0a3c69fec36bcff050791ebf9cba3b
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-09 23:32:04 +0000

    hotfix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,11 +28,13 @@ class LoginController extends Controller
     // protected $redirectTo = '/browse-jobs';
     public function redirectTo()
     {
-        if (auth()->user()->hasRole('freelancer')) {
-            return '/browse-jobs';
-        } else {
-            return '/browse-freelancers';
-        }
+        // if (auth()->user()->hasRole('freelancer')) {
+        //     return '/browse-jobs';
+        // } else {
+        //     return '/browse-freelancers';
+        // }
+
+        return 'dashboard';
     }
 
     /**
