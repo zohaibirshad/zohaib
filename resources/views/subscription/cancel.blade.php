61a68e3f9e9117f346bb68a24f126eed3a25f1c8
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-10 10:59:03 +0000

    hotfix

--- resources/views/subscription/cancel.blade.php
+++ resources/views/subscription/cancel.blade.php
@@ -13,6 +13,14 @@
 				<div class="mx-3 px-4 py-3 bg-orange-500 shadow-lg text-white"><h3 class="text-white">Bids Used: {{ $my_plan->pivot->count ?? '' }}</h3></div>
 				<div class="mx-3"><a href="../cancel/subscription"><button class=" bg-light-blue-500 shadow text-white px-16 text-lg py-3 font-bold rounded-full">CANCEL</button></a></div>
 			</div>
+			<p>10 free bids are added to your subscriptions every month by default</p>
+			<ul>
+			<li>Free Plan - 10 free bids</li>
+			<li>Economy Plus Plan - 10 additional bids + 10 free bids</li>
+			<li>Business Plan - 20 additional bids + 10 free bids</li>
+			<li>First Class Plan - 30 additional bids + 10 free bids</li>
+			</ul>
+			<p>Unused Bids for any plan are removed at that end of each month</p>
 			@endisset
 			@empty($my_plan->pivot)
 			<p>You are not subscribe to any plans yet.. <a href="../pricing">Subscibe now</a>
