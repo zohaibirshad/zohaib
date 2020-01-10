ff567e523b25900e266f73f1361b2818d878e3d3
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-10 15:04:55 +0000

    hotfix

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -14,11 +14,11 @@
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('A fresh verification link has been sent to your email inbox or spam.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('Before proceeding, please check your email inbox or spam for a verification link.') }}
                     {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
                 </div>
             </div>
