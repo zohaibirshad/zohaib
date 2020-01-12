1f4cac9ca236f1ed306fec9367bcc44745745c5b
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-12 16:27:49 +0000

    hotfix

--- resources/views/partials/alert.blade.php
+++ resources/views/partials/alert.blade.php
@@ -25,20 +25,6 @@
 @endif
 
 @auth
-@role('hirer')
-@if(auth()->user()->review == 'not_started')
-<div class="notification error closeable">
-    <p class="text-center" ><i class="icon-line-awesome-exclamation-circle" style="font-size: 20px;"></i>Please upload supporting documents to be fully verified<a href="../verify-profile"> Verify Now!</a></p>
-    <a class="close" href="#"></a>
-</div>
-@elseif(auth()->user()->review == 'pending')
-<div class="notification error closeable">
-    <p class="text-center" ><i class="icon-line-awesome-exclamation-circle" style="font-size: 20px;"></i>Your uploaded Documents are under review. Thank you!</a></p>
-    <a class="close" href="#"></a>
-</div>
-@endif
-@endrole
-
 @role('freelancer')
 @if(auth()->user()->review == 'not_started')
 <div class="notification error closeable">
