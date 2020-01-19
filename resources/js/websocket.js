871c41ebcd4280beebb3e41206992df8f5dec212
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-19 18:02:05 +0000

    instant messaging

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -18,7 +18,6 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
     disableStats: true,
 });
 
