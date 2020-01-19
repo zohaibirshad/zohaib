4e886a4136db096e16e0fc4700330f7f65f0c2ba
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-19 18:04:00 +0000

    instant messaging

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -15,7 +15,7 @@ window.VueChatScroll = VueChatScroll
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
