2bea6725c65d3bc8fc652dfdfc6b31f4a9556114
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-20 15:08:02 +0000

    instant messaging

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -17,7 +17,6 @@ let isProduction = process.env.MIX_WS_CONNECT_PRODUCTION === 'true';
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wssHost: window.location.hostname,
     wssPort: isProduction ? 6002 : 6001,
