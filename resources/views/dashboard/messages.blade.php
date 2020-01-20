8f23f018e5fc17139a29c581cc3e73755e94af22
Emmanuel Oduro <emmarthurson@gmail.com>
2020-01-20 13:13:11 +0000

    instant messaging

--- resources/views/dashboard/messages.blade.php
+++ resources/views/dashboard/messages.blade.php
@@ -218,7 +218,7 @@ const app = window.app = new Vue({
 			{
 				this.markSeen(this.single_conversation[0].conversation_id);
 				Echo.private('chat-conversation.' + this.single_conversation[0].conversation_id)
-					.listen('MessageWasSent', function(e) {
+					.listen('.MessageWasSent', function(e) {
 						self.single_conversation.push(e.message);
 						console.log(['websocket', e]);
 						
@@ -255,7 +255,7 @@ const app = window.app = new Vue({
 			{
 				this.markSeen(this.single_conversation[0].conversation_id);
 				Echo.private('chat-conversation.' + this.single_conversation[0].conversation_id)
-				.listen('MessageWasSent', function(e) {
+				.listen('.MessageWasSent', function(e) {
 					self.single_conversation.push(e.message);
 					console.log(['websocket', e]);
 
