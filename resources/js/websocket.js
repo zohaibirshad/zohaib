fa61f3f9f57ad5f20e0ca09d4a6e768905c9eb16
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-11 22:35:51 +0000

    hotfix

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -15,12 +15,12 @@ window.VueChatScroll = VueChatScroll;
 let isProduction = process.env.MIX_WS_CONNECT_PRODUCTION === 'true';
 
 window.Echo = new Echo({
+    broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: isProduction ? window.location.hostname : 'websocket.yohli.com',
-    wsPort: isProduction ? 6001 : 6002,
-    wssHost: isProduction ? window.location.hostname : 'websocket.yohli.com',
-    wssPort: isProduction ? 6001 : 6002,
+    wsPort: isProduction ? 6001 : 6001,
     disableStats: true,
     encrypted: true,
+    namespace: 'App.Events'
 });
 
