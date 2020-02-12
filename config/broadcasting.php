6faa5df69e2181c601d964e8e99e907ce44f69c4
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-12 10:01:29 +0000

    hotfix

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,6 +30,22 @@ return [
 
     'connections' => [
         
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'host' => env('WEBSOCKET_HOST'),
+        //         'port' => env('MIX_WS_CONNECT_PRODUCTION') ? 6001 : 6002,
+        //         'scheme' => 'https',
+        //         'curl_options' => [
+        //             CURLOPT_SSL_VERIFYHOST => env('CURLOPT_SSL_VERIFYHOST', 0),
+        //             CURLOPT_SSL_VERIFYPEER => env('CURLOPT_SSL_VERIFYPEER', 0),
+        //         ]
+        //     ],
+        // ],
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -37,15 +53,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('WEBSOCKET_HOST'),
-                'port' => env('MIX_WS_CONNECT_PRODUCTION') ? 6001 : 6002,
-                'scheme' => 'https',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => env('CURLOPT_SSL_VERIFYHOST', 0),
-                    CURLOPT_SSL_VERIFYPEER => env('CURLOPT_SSL_VERIFYPEER', 0),
-                ]
+                'encrypted' => true,
             ],
         ],
+
 
         'redis' => [
             'driver' => 'redis',
--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -17,12 +17,7 @@ let isProduction = process.env.MIX_WS_CONNECT_PRODUCTION === 'true';
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: isProduction ? window.location.hostname : 'websocket.yohli.com',
-    wsPort: isProduction ? 6001 : 6002,
-    wssHost: isProduction ? window.location.hostname : 'websocket.yohli.com',
-    wssPort: isProduction ? 6001 : 6002,
-    disableStats: true,
-    encrypted: true,
-    namespace: 'App.Events'
+    cluster: 'mt1',
+    forceTLS: true,
 });
 
