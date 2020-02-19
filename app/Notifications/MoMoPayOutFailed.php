43659fccc39543ecac505309285ca94c7803bce9
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-19 16:19:31 +0000

    hotfix

--- app/Notifications/MoMoPayOutFailed.php
+++ app/Notifications/MoMoPayOutFailed.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class MoMoPayOutFailed extends Notification
+class MoMoPayOutFailed extends Notification implements ShouldQueue
 {
     use Queueable;
 
