063857f2f9f92daf4870df3c59712aa4dc424952
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-19 19:21:25 +0000

    hotfix

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -46,18 +46,7 @@ class PayPalService {
              $this->secret
             )
         );
-        $this->apiContext->setConfig(
-            array(
-                'mode' => 'sandbox',
-                'log.LogEnabled' => true,
-                'log.FileName' => storage_path() . '/logs/paypal.log',
-                'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
-                //'cache.FileName' => '/PaypalCache' // for determining paypal cache directory
-                // 'http.CURLOPT_CONNECTTIMEOUT' => 30
-                // 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
-                //'log.AdapterFactory' => '\PayPal\Log\DefaultLogFactory' // Factory class implementing \PayPal\Log\PayPalLogFactory
-            )
-        );
+        $this->apiContext->setConfig(config('paypal.settings') );
 
 
     }
