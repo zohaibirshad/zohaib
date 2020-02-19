2869d8039af230c9d068c27f223f5bb9f5b44f1f
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-19 22:07:06 +0000

    hotfix

--- resources/views/subscription/cancel.blade.php
+++ resources/views/subscription/cancel.blade.php
@@ -7,12 +7,14 @@
 		<div class="col-xl-12">
 			@auth
 			<!-- Billing Cycle  -->
-			<div class="margin-bottom-70 flex flex-row">
+			<div class="margin-bottom-70 flex flex-row flex-wrap justify-start">
 			@isset($my_plan->pivot)
-				<div class="mx-3 px-4 py-3 bg-orange-500 shadow-lg text-white"><h3 class="text-white ">Current Plan: {{ $my_plan->title ?? '' }} - Bids {{ $my_plan->quantity ?? '' }}</h3></div>
-				<div class="mx-3 px-4 py-3 bg-orange-500 shadow-lg text-white"><h3 class="text-white">Bids Used: {{ $my_plan->pivot->count ?? '' }}</h3></div>
+				<div class="flex flex-row ">
+					<div class="m-1 px-4 pt-3 pb-0 mb-0 bg-orange-500 shadow text-white rounded-lg"><p class="text-white ">{{ $my_plan->title ?? '' }} - Bids {{ $my_plan->quantity ?? '' }}</p></div>
+					<div class="m-1 px-4 pt-3 pb-0 mb-0 bg-orange-500 shadow text-white rounded-lg"><p class="text-white">Bids Used: {{ $my_plan->pivot->count ?? '' }}</p></div>
+				</div>
 				@if($my_plan->plan_id != 'free')
-				<div class="mx-3"><a onclick="cancel()" href="#"><button class=" bg-light-blue-500 shadow text-white px-16 text-lg py-3 font-bold rounded-full">CANCEL</button></a></div>
+				<div class=""><a onclick="cancel()" href="#"><button class="m-1 bg-light-blue-500 shadow text-white px-16 py-3 font-bold rounded-full">CANCEL</button></a></div>
 				@endif
 			</div>
 			<p>10 free bids are added to your subscriptions every month by default</p>
