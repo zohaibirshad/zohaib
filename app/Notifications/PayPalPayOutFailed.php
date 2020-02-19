4e25958ad228f9316230df2fc7e7918b8247baa0
Emmanuel Oduro <emmarthurson@gmail.com>
2020-02-19 15:59:51 +0000

    hotfix

--- app/Notifications/PayPalPayOutFailed.php
+++ app/Notifications/PayPalPayOutFailed.php
@@ -2,10 +2,11 @@
 
 namespace App\Notifications;
 
+use App\Models\Transaction;
 use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
 class PayPalPayOutFailed extends Notification implements ShouldQueue
 {
